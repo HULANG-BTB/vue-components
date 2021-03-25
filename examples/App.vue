ce5d47ed28343ef1b33641e832ade9784afdc2cb
AloneH <admin@oibit.cn>
2021-03-25 11:10:32 +0800

    feat: initlization

---
+++ examples/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="#app">example</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'App'
+})
+</script>
---
+++ examples/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const examples = createApp(App)
+examples.mount('#app')
+
+export default examples
