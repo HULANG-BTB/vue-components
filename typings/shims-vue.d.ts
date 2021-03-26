f3306ad2a15922b7e7f71f7af6ac8d534888e370
AloneH <admin@oibit.cn>
2021-03-26 10:04:48 +0800

    refactor: types declare for vue

--- typings/shims-vue.d.ts
+++ typings/shims-vue.d.ts
@@ -1,6 +1,7 @@
-/* eslint-disable */
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
+  import { App, defineComponent } from 'vue'
+  const component: ReturnType<typeof defineComponent> & {
+    install(app: App): void
+  }
   export default component
 }
