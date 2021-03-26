e912cf405c8a35f9c2ee171c057fd6e2fcdc2fbd
AloneH <admin@oibit.cn>
2021-03-26 11:51:03 +0800

    chore: build tools for theme

--- build/gulp.config.theme.js
+++ build/gulp.config.theme.js
@@ -4,16 +4,17 @@ const { series, src, dest } = require('gulp')
 const sass = require('gulp-dart-sass')
 const autoprefixer = require('gulp-autoprefixer')
 const cssmin = require('gulp-cssmin')
+const path = require('path')
 // const rename = require('gulp-rename')
 
 // const noElPrefixFile = /(index|base|display)/
 
 function compile() {
-  return src('./src/*.scss')
+  return src(path.resolve(__dirname, '../packages/theme-chalk/src/*.scss'))
     .pipe(sass.sync())
     .pipe(autoprefixer({ cascade: false }))
     .pipe(cssmin())
-    .pipe(dest('./lib'))
+    .pipe(dest(path.resolve(__dirname, '../lib/theme-chalk/')))
   // .pipe(
   //   rename(function (path) {
   //     if (!noElPrefixFile.test(path.basename)) {
@@ -24,7 +25,9 @@ function compile() {
 }
 
 function copyfont() {
-  return src('./src/fonts/**').pipe(cssmin()).pipe(dest('./lib/fonts'))
+  return src(path.resolve(__dirname, '../packages/theme-chalk/src/fonts/**'))
+    .pipe(cssmin())
+    .pipe(dest(path.resolve(__dirname, '../packages/theme-chalk/fonts')))
 }
 
 exports.build = series(compile, copyfont)
